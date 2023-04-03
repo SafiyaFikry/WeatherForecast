d6876bed3fa803ec4a1b17305a3535ca8d9ea34f
safiyafikry <safiyafikry@gmail.com>
2023-04-03 20:18:25 +0200

    idrequest is added

--- app/src/main/java/eg/gov/iti/jets/weatherapp/alertScreen/view/AlertFragment.kt
+++ app/src/main/java/eg/gov/iti/jets/weatherapp/alertScreen/view/AlertFragment.kt
@@ -39,20 +39,20 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class AlertFragment : Fragment() {
-    lateinit var binding:FragmentAlertBinding
+    lateinit var binding: FragmentAlertBinding
     lateinit var alertsViewModel: ViewModelAlerts
     lateinit var alertsFactory: ViewModelFactoryAlerts
     lateinit var alertsAdapter: AlertsAdapter
     lateinit var homeFactory: ViewModelFactoryHome
     lateinit var homeViewModel: ViewModelHome
-    var timestamp=0
-    lateinit var dateTime:String
-    var startDate=0L
-    var endDate=0L
-    val c =Calendar.getInstance()
-    lateinit var address:MutableList<Address>
+    var timestamp = 0
+    lateinit var dateTime: String
+    var startDate = 0L
+    var endDate = 0L
+    val c = Calendar.getInstance()
+    lateinit var address: MutableList<Address>
     lateinit var geocoder: Geocoder
-    lateinit var des:String
+    lateinit var des: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -62,81 +62,60 @@ class AlertFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding= FragmentAlertBinding.inflate(inflater,container,false)
+        binding = FragmentAlertBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        geocoder= Geocoder(requireContext().applicationContext)
-        alertsFactory= ViewModelFactoryAlerts(
+        geocoder = Geocoder(requireContext().applicationContext)
+        alertsFactory = ViewModelFactoryAlerts(
             Repository.getInstance(
                 WeatherClient.getInstance(),
-            ConcreteLocalSource(requireContext().applicationContext)
-        ))
-        alertsViewModel= ViewModelProvider(this,alertsFactory).get(ViewModelAlerts::class.java)
+                ConcreteLocalSource(requireContext().applicationContext)
+            )
+        )
+        alertsViewModel = ViewModelProvider(this, alertsFactory).get(ViewModelAlerts::class.java)
 
-        homeFactory = ViewModelFactoryHome(Repository.getInstance(WeatherClient.getInstance(),
-            ConcreteLocalSource(requireContext().applicationContext)
-        ))
+        homeFactory = ViewModelFactoryHome(
+            Repository.getInstance(
+                WeatherClient.getInstance(),
+                ConcreteLocalSource(requireContext().applicationContext)
+            )
+        )
         homeViewModel = ViewModelProvider(this, homeFactory).get(ViewModelHome::class.java)
 
-        alertsViewModel.alertsDB.observe(viewLifecycleOwner){alerts->
+        alertsViewModel.alertsDB.observe(viewLifecycleOwner) { alerts ->
 
-            binding.imageView.visibility=View.GONE
-            binding.textView.visibility=View.GONE
-            alertsAdapter= AlertsAdapter(alerts){
-                cancelAlarm()
+            binding.imageView.visibility = View.GONE
+            binding.textView.visibility = View.GONE
+            alertsAdapter = AlertsAdapter(alerts) {
+                cancelAlarm(it.id)
                 alertsViewModel.deleteAlert(it)
                 alertsAdapter.notifyDataSetChanged()
             }
-            binding.alertRecyclerView.adapter=alertsAdapter
+            binding.alertRecyclerView.adapter = alertsAdapter
             alertsAdapter.notifyDataSetChanged()
 
-            if(alerts.size==0){
-                binding.imageView.visibility=View.VISIBLE
-                binding.textView.visibility=View.VISIBLE
+            if (alerts.size == 0) {
+                binding.imageView.visibility = View.VISIBLE
+                binding.textView.visibility = View.VISIBLE
             }
         }
-        binding.alertFloatingActionButton.setOnClickListener{
-            val notifications=shared.getBoolean("notifications",false)
-            if (notifications==true) {
+        binding.alertFloatingActionButton.setOnClickListener {
+            val notifications = shared.getBoolean("notifications", false)
+            if (notifications == true) {
                 showRadioConfirmationDialog()
-            }
-            else{
-                Toast.makeText(requireContext(),"Enable Notifications in Settings",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Enable Notifications in Settings",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
 
     }
-    /*fun openDialog(){
-
-        DatePickerDialog(requireContext(), { view, year, month, dayOfMonth ->
-            dateTime="Date: "
-            dateTime+=dayOfMonth.toString()+"/"+month.toString()+"/"+year.toString()+"\n"
-
-            TimePickerDialog(requireContext(),{ view, hourOfDay, minute ->
-                //check time ******************
-                if (hourOfDay>12) {
-                    dateTime +="Time: "+ (hourOfDay-12).toString() + ":" + minute.toString()+" PM"
-                }
-                else{
-                    dateTime +="Time: "+ (hourOfDay).toString() + ":" + minute.toString()+" AM"
-                }
-                c.set(Calendar.HOUR_OF_DAY,hourOfDay)
-                c.set(Calendar.MINUTE,minute)
-                c.set(Calendar.SECOND,0)
-                timestamp=c.timeInMillis.toInt()
-                println("#######$dateTime ######### $timestamp")
-                val editor=shared.edit()
-                editor.putString("dateTime",dateTime)
-                editor.putInt("timestamp",timestamp)
-                editor.commit()
-
-            },Calendar.getInstance().get(Calendar.HOUR),Calendar.getInstance().get(Calendar.MINUTE),false).show()
-
-       },Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH),Calendar.getInstance().get(Calendar.DAY_OF_MONTH)).show()
-
-    }*/
 
     private fun setDate(callback: (Long) -> Unit) {
         Calendar.getInstance().apply {
@@ -172,45 +151,49 @@ class AlertFragment : Fragment() {
             datePickerDialog.show()
         }
     }
+
     fun showRadioConfirmationDialog() {
-        var selectedOptionIndex= 0
+        var selectedOptionIndex = 0
         val option = arrayOf("Alert", "Notification")
         var selectedOption = option[selectedOptionIndex]
         val inflater = requireActivity().layoutInflater
-        val myView=inflater.inflate(R.layout.alert_dialog,null)
-        val builder=MaterialAlertDialogBuilder(requireContext())
+        val myView = inflater.inflate(R.layout.alert_dialog, null)
+        val builder = MaterialAlertDialogBuilder(requireContext())
         builder.setTitle("Type of Alert?")
             .setView(myView)
-        val start_ed=myView.findViewById<Button>(R.id.startDate_Btn)
+        val start_ed = myView.findViewById<Button>(R.id.startDate_Btn)
         start_ed.setOnClickListener {
-            setDate{startDate=it
-                start_ed.text= SimpleDateFormat("dd/MM/YYYY hh:mm a").format(Date(startDate))
+            setDate {
+                startDate = it
+                start_ed.text = SimpleDateFormat("dd/MM/YYYY hh:mm a").format(Date(startDate))
             }
         }
-        val end_ed=myView.findViewById<Button>(R.id.endDate_Btn)
+        val end_ed = myView.findViewById<Button>(R.id.endDate_Btn)
         end_ed.setOnClickListener {
-            setDate{endDate=it
-                end_ed.text=SimpleDateFormat("dd/MM/YYYY hh:mm a").format(Date(endDate))}
+            setDate {
+                endDate = it
+                end_ed.text = SimpleDateFormat("dd/MM/YYYY hh:mm a").format(Date(endDate))
+            }
 
         }
         builder.setSingleChoiceItems(option, selectedOptionIndex) { dialog_, which ->
             selectedOptionIndex = which
             selectedOption = option[which]
         }
-        .setPositiveButton("Ok") { dialog, which ->
+            .setPositiveButton("Ok") { dialog, which ->
 
-            if (!Settings.canDrawOverlays(requireContext())) {
-                Toast.makeText(
-                    requireContext(),
-                    "The alarm may be not work as expected",
-                    Toast.LENGTH_SHORT
-                ).show()
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + requireContext().packageName)
-                )
-                someActivityResultLauncher.launch(intent)
-            }
+                if (!Settings.canDrawOverlays(requireContext())) {
+                    Toast.makeText(
+                        requireContext(),
+                        "The alarm may be not work as expected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val intent = Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + requireContext().packageName)
+                    )
+                    someActivityResultLauncher.launch(intent)
+                }
                 val lat = shared.getString("lat", "33.44")
                 val lon = shared.getString("lon", "-94.04")
                 address = geocoder.getFromLocation(
@@ -241,7 +224,12 @@ class AlertFragment : Fragment() {
                                 type = "alert"
                             )
                         )
-                        startAlarm(startDate, "alert")
+                        //startAlarm(startDate, "alert",alertsViewModel.alertsDB.value!!.size+1)
+                        startAlarm(
+                            startDate,
+                            "alert",
+                            alertsViewModel.alertsDB.value!![alertsViewModel.alertsDB.value!!.size-1].id+1
+                        )
                     } else {
                         alertsViewModel.insertAlert(
                             AlertsDB(
@@ -257,13 +245,19 @@ class AlertFragment : Fragment() {
                                 type = "notification"
                             )
                         )
-                        startAlarm(startDate, "notification")
+                        //startAlarm(startDate, "notification",alertsViewModel.alertsDB.value!!.size+1)
+                        startAlarm(
+                            startDate,
+                            "notification",
+                            alertsViewModel.alertsDB.value!![alertsViewModel.alertsDB.value!!.size - 1].id+1
+                        )
                     }
                 }
             }
 
-        .create().show()
+            .create().show()
     }
+
     private val someActivityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (!Settings.canDrawOverlays(requireContext())) {
@@ -274,18 +268,24 @@ class AlertFragment : Fragment() {
                 ).show()
             }
         }
-    fun startAlarm(time:Long,type:String){
+
+    fun startAlarm(time: Long, type: String, id: Int) {
         var alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent= Intent(requireContext(),AlertReceiver::class.java)
-        intent.putExtra("type",type)
-        var pendingIntent:PendingIntent= PendingIntent.getBroadcast(requireContext(),1,intent,PendingIntent.FLAG_IMMUTABLE)
+        val intent = Intent(requireContext(), AlertReceiver::class.java)
+        intent.putExtra("type", type)
+        var pendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(requireContext(), id, intent, PendingIntent.FLAG_IMMUTABLE)
+        println("#################### start id $id")
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, pendingIntent)
 
     }
-    fun cancelAlarm(){
+
+    fun cancelAlarm(id: Int) {
         var alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent= Intent(requireContext(),AlertReceiver::class.java)
-        var pendingIntent:PendingIntent= PendingIntent.getBroadcast(requireContext(),1,intent,PendingIntent.FLAG_IMMUTABLE)
+        val intent = Intent(requireContext(), AlertReceiver::class.java)
+        var pendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(requireContext(), id, intent, PendingIntent.FLAG_IMMUTABLE)
+        println("++++++++++++++++++ end id $id")
         alarmManager.cancel(pendingIntent)
     }
 
