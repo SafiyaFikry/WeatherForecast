30b1c7ac7f19a6bc272724a866d4da9d27deb729
safiyafikry <safiyafikry@gmail.com>
2023-04-05 15:38:31 +0200

    nothing

--- app/src/main/java/eg/gov/iti/jets/weatherapp/favoriteScreen/view/FavoriteFragment.kt
+++ app/src/main/java/eg/gov/iti/jets/weatherapp/favoriteScreen/view/FavoriteFragment.kt
@@ -3,6 +3,8 @@ package eg.gov.iti.jets.weatherapp.favoriteScreen.view
 import android.content.Context
 import android.content.DialogInterface
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -71,9 +73,14 @@ class FavoriteFragment : Fragment() {
             }
         }
         binding.favoriteFloatingActionButton.setOnClickListener{
-            //viewModel.setDes("fav")
-            ViewModelFavorite.destination="fav"
-            Navigation.findNavController(it).navigate(R.id.mapFragment)
+            if(checkForInternet(requireContext())) {
+                //viewModel.setDes("fav")
+                ViewModelFavorite.destination = "fav"
+                Navigation.findNavController(it).navigate(R.id.mapFragment)
+            }
+            else{
+                Toast.makeText(requireContext(),"No Network",Toast.LENGTH_LONG).show()
+            }
         }
     }
     fun showConfirmationDialog(favoritesDB: FavoritesDB) {
@@ -90,5 +97,15 @@ class FavoriteFragment : Fragment() {
 
             }
             .create().show()
+    }
+    private fun checkForInternet(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
     }
 }
